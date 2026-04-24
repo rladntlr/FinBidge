26331d86bee0361024fd9b62002c27534fe6b4fe
rladntlr <tlrrns08@gmail.com>
2026-04-24 13:04:02 +0900

    Initial commit: 프로젝트 초기 설정

---
+++ src/main/java/springboot/finbridge/FinBridgeApplication.java
@@ -0,0 +1,13 @@
+package springboot.finbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinBridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinBridgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springboot/finbridge/FinBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.finbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinBridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
